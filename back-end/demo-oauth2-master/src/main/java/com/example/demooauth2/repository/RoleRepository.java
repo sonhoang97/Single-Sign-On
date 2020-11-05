2343228f08ef14acfcb7f2ee8660105b40286ace
LƯU NGUYỄN HOÀNG <luuhoang231@gmail.com>
2020-11-05 16:54:54 +0700

    custom repository

--- back-end/demo-oauth2-master/src/main/java/com/example/demooauth2/repository/RoleRepository.java
+++ back-end/demo-oauth2-master/src/main/java/com/example/demooauth2/repository/RoleRepository.java
@@ -2,9 +2,14 @@ package com.example.demooauth2.repository;
 
 import com.example.demooauth2.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    @Query("select r from Role r where r.name =?1")
     Optional<Role> findByName(String roleName);
+
+
 }
